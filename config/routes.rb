ad0d49dd9ae7711cbcacf68e7c8fd231f35197de
Tyler Rick <tyler@tylerrick.com>
2019-04-02 00:24:26 -0700

    f debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
-byebug
 RackAttackAdmin::Engine.routes.draw do
   root to: 'rack_attack#index'
   get :current_request, to: 'rack_attack#current_request'
   resources :banned_ips, only: [:create, :destroy], id: /.*/
   resources :keys, only: [:destroy], id: /.*/
-end
+end if false
