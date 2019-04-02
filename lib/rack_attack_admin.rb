4cee94220640fd13297a0e8088d9a286af9fb86e
Tyler Rick <tyler@tylerrick.com>
2019-04-01 22:47:26 -0700

    Initial commit

---
+++ lib/rack_attack_admin.rb
@@ -0,0 +1,6 @@
+require "rack_attack_admin/version"
+
+module RackAttackAdmin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rack_attack_admin_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RackAttackAdmin do
+  it "has a version number" do
+    expect(RackAttackAdmin::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
