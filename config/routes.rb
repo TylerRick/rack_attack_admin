3870fc1c61e72dfa9e4ddb5e6f18b2fd63d1e04a
Tyler Rick <tyler@tylerrick.com>
2019-04-01 22:47:26 -0700

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+RackAttackAdmin::Engine.routes.draw do
+  root to: 'rack_attack#index'
+  get :current_request, to: 'rack_attack#current_request'
+  resources :banned_ips, only: [:create, :destroy], id: /.*/
+  resources :keys, only: [:destroy], id: /.*/
+end
