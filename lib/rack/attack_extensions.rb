4f1e6037185d1ae1eba445bec6a48d284d3a7b19
Tyler Rick <tyler@tylerrick.com>
2019-06-17 13:00:28 -0700

    f

--- lib/rack/attack_extensions.rb
+++ lib/rack/attack_extensions.rb
@@ -17,7 +17,7 @@ class Rack::Attack
 
     # The same as cache.prefix but prefixed with "{namespace}:" if namespace option is set.
     # Like cache.prefix, this does not include the trailing ':'.
-    def prefix_with_namespace
+    def cache.prefix
       prefix = cache.prefix
       if namespace = cache.store&.options&.[](:namespace)
         prefix = "#{namespace}:#{prefix}"
@@ -26,7 +26,7 @@ class Rack::Attack
     end
 
     def prefixed_keys
-      all_keys.grep(/^#{prefix_with_namespace}:/)
+      all_keys.grep(/^#{cache.prefix}:/)
     end
 
     # AKA unprefixed_keys
@@ -37,7 +37,7 @@ class Rack::Attack
     end
 
     def unprefix_key(key)
-      key.sub "#{prefix_with_namespace}:", ''
+      key.sub "#{cache.prefix}:", ''
     end
 
     def to_h
@@ -250,7 +250,7 @@ class Rack::Attack
 
     class << self
       def prefixed_keys
-        Rack::Attack.all_keys.grep(/^#{Rack::Attack.prefix_with_namespace}:(allow|fail)2ban:/)
+        Rack::Attack.all_keys.grep(/^#{cache.prefix}:(allow|fail)2ban:/)
       end
 
       # AKA unprefixed_keys
@@ -272,7 +272,7 @@ class Rack::Attack
       end
 
       def full_key_prefix
-        "#{Rack::Attack.prefix_with_namespace}:#{key_prefix}"
+        "#{cache.prefix}:#{key_prefix}"
       end
     end
   end
@@ -322,7 +322,7 @@ class Rack::Attack
       end
 
       def full_key_prefix
-        "#{Rack::Attack.prefix_with_namespace}:#{key_prefix}"
+        "#{cache.prefix}:#{key_prefix}"
       end
 
     protected
